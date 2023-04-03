1e16e0714e319b845ff5828c45ec4d251d0c4e11
whk06061 <gju06061@naver.com>
2023-04-04 04:55:40 +0900

    refactor: 이동 거리 setter를 private으로 변경

--- src/main/kotlin/domain/Car.kt
+++ src/main/kotlin/domain/Car.kt
@@ -4,7 +4,9 @@ private const val MINIMUM_MOVE_VALUE = 4
 private const val NAME_LENGTH_ERROR = "이름은 다섯글자를 초과하면 안됩니다."
 private const val MAX_NAME_LENGTH = 5
 
-class Car(val name: String, var distance: Int = 0) {
+class Car(val name: String, distance: Int = 0) {
+    var distance = distance
+        private set
 
     init {
         require(name.length <= MAX_NAME_LENGTH) { NAME_LENGTH_ERROR }
